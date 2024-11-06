d0439bddc5a606974a3243ba6155175ffc8fa849
Icy-Joker <790238255@qq.com>
2024-11-06 14:44:56 +0800

    do_synchronization.sh

--- do_synchronization.sh
+++ do_synchronization.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 #
-cp ~/.bashrc ~/.bash_profile ~/.gitconfig ~/.inputrc ~/.vimrc ~/.zprofile ~/.zshenv ~/.zshrc .
+file_names="$HOME/.bashrc;$HOME/.bash_profile;$HOME/.gitconfig;$HOME/.inputrc;$HOME/.vimrc;$HOME/.zprofile;$HOME/.zshenv;$HOME/.zshrc"
+# 设置 IFS 为分号
+IFS=';'
+# 逐个输出字符串中分号隔开的部分
+for file_name in $file_names; do
+  echo "$file_name"
+  ln -f "$file_name" .
+done
+# 恢复 IFS 为默认空格
+unset IFS
